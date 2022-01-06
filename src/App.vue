6fd2a8ce604d97015ca779b9ed554e1bdd9144ab
torish14 <nanamin.104@icloud.com>
2022-01-06 11:29:58 +0900

    ✨ feat: 型宣言した job.ts をインポートして新しいプロパティを作成

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,51 @@
 <template>
   <div class="app">
     <h1>Hello Vue!</h1>
-    <p>
+    <!-- <p>
       {{ name }} - {{ age }}
     </p>
     <button @click="changeName('zelda')">change name</button>
-    <button @click="changeAge(20)">change age</button>
+    <button @click="changeAge(20)">change age</button> -->
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import Job from './types/job'
 
 export default defineComponent({
   name: 'App',
   components: {},
   setup() {
     /* const state = reactive({ */
-      /* name: 'link', */
-      /* age: 25 as string | number */
+    /* name: 'link', */
+    /* age: 25 as string | number */
     /* }) */
 
     /* return{ ...toRefs(state) } */
 
-    const name = ref('link')
-    const age = ref<number | string>(25)
+    /* const name = ref('link') */
+    /* const age = ref<number | string>(25) */
 
-    return { name, age }
+    /* return { name, age } */
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'young america', salary: 10000, id: 1 },
+      { title: 'farmer', location: 'new zealand', salary: 20000, id: 2 },
+      { title: 'doctor', location: 'united states', salary: 30000, id: 3 },
+    ])
+    return { jobs }
   },
-  methods: {
-    changeName(name: string) {
-      this.name = name
-      return name
-    },
-    changeAge(age: number | string) {
-      this.age = age
-      return age
-    },
-  },
+  /* methods: { */
+  /* changeName(name: string) { */
+  /* this.name = name */
+  /* return name */
+  /* }, */
+  /* changeAge(age: number | string) { */
+  /* this.age = age */
+  /* return age */
+  /* }, */
+  /* }, */
 })
 </script>
 
